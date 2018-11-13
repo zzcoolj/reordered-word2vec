559e088f15e9ed4ceb5a4bf86985e1a38c035f15
Zheng ZHANG <zzcoolj@client-172-18-76-95.clients.u-psud.fr>
2018-11-13 16:26:59 +0100

    evaluations all

--- corpus_preprocessor.py
+++ corpus_preprocessor.py
@@ -62,14 +62,18 @@ def evaluate(vec):
     return results2 + results3 + results1
 
 
-start = time.time()
-# train_models('input/enwiki-101M.txt', 'output/test101M-vocab20000-restricted')
-train_models('input/enwiki-1G.txt', 'output/test1G-vocab50000-analogy&353&999')
-end = time.time()
-print('time (seconds):', end-start)
+# start = time.time()
+# # train_models('input/enwiki-101M.txt', 'output/test101M-vocab20000-restricted')
+# train_models('input/enwiki-1G.txt', 'output/test1G-vocab50000-analogy&353&999')
+# end = time.time()
+# print('time (seconds):', end-start)
 
 
-# vec = KeyedVectors.load_word2vec_format('output/test').wv
-# print(evaluate(vec))
-# vec = KeyedVectors.load_word2vec_format('output/test101M-vocab20000-restricted').wv
-# print(evaluate(vec))
+vec = KeyedVectors.load_word2vec_format('output/test1G-vocab50000-353').wv
+print(evaluate(vec))
+vec = KeyedVectors.load_word2vec_format('output/test1G-vocab50000-999').wv
+print(evaluate(vec))
+vec = KeyedVectors.load_word2vec_format('output/test1G-vocab50000-analogy').wv
+print(evaluate(vec))
+vec = KeyedVectors.load_word2vec_format('output/test1G-vocab50000-analogy&353&999').wv
+print(evaluate(vec))
