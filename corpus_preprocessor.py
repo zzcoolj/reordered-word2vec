23b0f32fd3229227ad7b2181e8e00561e88a2b0a
Zheng ZHANG <zzcoolj@client-172-18-84-138.clients.u-psud.fr>
2018-12-05 16:07:55 +0100

    alpha splitter

--- corpus_preprocessor.py
+++ corpus_preprocessor.py
@@ -32,6 +32,21 @@ def evaluate(vec, name):
     return [name] + results2 + results3 + results4 + results1
 
 
+def alpha_splitter(start, epochs, end=0.0001):
+    alphas = list()
+    alphas.append(start)
+    for i in range(1, epochs-1):
+        alpha = start - ((start - end) * float(i) / epochs)
+        alphas.append(alpha)
+    alphas.append(end)
+    return alphas
+
+
+total_epoch = 4
+alphas = alpha_splitter(start=0.05, epochs=total_epoch)  # TODO NOW
+print('alphas', alphas)
+
+
 lr = 0.05
 dim = 200
 ws = 5
@@ -42,9 +57,7 @@ neg = 5
 loss = 'ns'
 t = 1e-4
 workers = 3  # 3 by default
-
-min_alpha_after_epochs = 0.0001  # TODO NOW
-min_alpha = lr - ((lr - min_alpha_after_epochs) * float(1) / 7)  # TODO NOW
+min_alpha = alphas[1]  # TODO NOW 0.0001
 
 # restricted_vocab = read_file_to_dict('../word_embeddings_evaluator/data/distinct-tokens/analogy&353&999.txt')
 restricted_vocab = read_file_to_dict('../word_embeddings_evaluator/data/distinct-tokens/353.txt')  # TODO NOW
@@ -53,6 +66,7 @@ restricted_type = 0  # TODO NOW
 # Same values as used for fastText training above
 params = {
     'alpha': lr,
+    'min_alpha': min_alpha,
     'size': dim,
     'window': ws,
     'iter': epoch,
@@ -63,9 +77,9 @@ params = {
     'hs': 0,  # If 0, and negative is non-zero, negative sampling will be used.
     'negative': neg,
     'workers': workers,
+
     'restricted_vocab': restricted_vocab,  # [modified] ATTENTION: It must be a dictionary not a list!
-    'restricted_type': restricted_type,  # [modified] 0: train_batch_sg_original; 1: train_batch_sg_in; 2: train_batch_sg_notIn
-    'min_alpha': min_alpha
+    'restricted_type': restricted_type  # [modified] 0: train_batch_sg_original; 1: train_batch_sg_in; 2: train_batch_sg_notIn
 }
 
 
@@ -76,8 +90,7 @@ params = {
 
 """ Epoch Simulation """
 corpus_file = 'input/enwiki-1G.txt'
-total_epoch = 7
-xlsx_path = 'output/test1G-vocab50000-original-iter7.xlsx'
+xlsx_path = 'output/test1G-vocab50000-original-iter4.xlsx'  # TODO NOW
 
 df = pd.DataFrame(columns=[
     # word embeddings file name
@@ -100,12 +113,12 @@ print('cur_epoch', 0)
 gs_model = Word2Vec(LineSentence(corpus_file), **params)
 df.loc[0] = evaluate(gs_model.wv, 'iter0')
 
-
 for cur_epoch in range(1, total_epoch):
-    # epoch 1-4
     print('cur_epoch', cur_epoch)
-    start_alpha = lr - ((lr - min_alpha_after_epochs) * float(cur_epoch) / total_epoch)
-    end_alpha = lr - ((lr - min_alpha_after_epochs) * float(cur_epoch+1) / total_epoch)
+    start_alpha = alphas[cur_epoch]
+    end_alpha = alphas[cur_epoch]
+    print('start_alpha', start_alpha)
+    print('end_alpha', end_alpha)
     gs_model.train(LineSentence(corpus_file), total_examples=gs_model.corpus_count, epochs=gs_model.iter,
                    start_alpha=start_alpha, end_alpha=end_alpha)
     df.loc[cur_epoch] = evaluate(gs_model.wv, 'iter'+str(cur_epoch))
